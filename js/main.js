6aee29a420b37c368107341b7b32dda6d5f0cdb5
druidmaciek <maciej@janowski.dev>
2021-09-20 12:02:16 -0500

    update sizes, spacing and smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 const COLORS = ['bg-indigo-300', 'bg-blue-300', 'bg-pink-300', 'bg-rose-300', 'bg-yellow-300', 'bg-orange-300', 'bg-purple-300']
 const ANIM = ['animation-delay-2000', 'animation-delay-4000', 'animation-delay-1000']
+import smoothscroll from 'smoothscroll-polyfill';
+
+// kick off the polyfill!
+smoothscroll.polyfill();
